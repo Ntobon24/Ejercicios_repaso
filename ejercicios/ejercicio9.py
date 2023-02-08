ead24dd42b85af734575641194e530d671518fef
Nicolas Tobon <nicolastobon242>
2023-02-08 18:59:41 -0500

    add ejercicio9 functional

--- ejercicios/ejercicio9.py
+++ ejercicios/ejercicio9.py
@@ -1,1 +1,10 @@
-#este despues
+import random
+
+filas = int(input("ingrese la cantidad de filas de la matriz: "))
+columnas = int(input("ingrese la cantidad de columnas de la matriz: "))
+range_max = int(input("ingrese el rango maximo de los numeros en la matriz: "))
+
+m = [[random.randint(1,range_max) for j in range(columnas)] for i in range(filas)]
+
+for f in m:
+    print(f)
