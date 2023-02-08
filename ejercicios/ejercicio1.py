f0818bf89cc1ef1376452b05df61d70b3fd3c029
Nicolas Tobon <nicolastobon242>
2023-02-08 14:46:10 -0500

    add ejercicio1 funtional

---
+++ ejercicios/ejercicio1.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    print()
+
+name = input("ingresa tu nombre: ")
+edad = (int(input("ingresa tu edad: ")))
+
+print(f"el nombre ingresado es {name} y la edad ingresada es de {edad} años")
+
