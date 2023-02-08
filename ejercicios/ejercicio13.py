997b19e89897bf74834c33ff7ed4645e08ebb824
Nicolas Tobon <nicolastobon242>
2023-02-08 17:36:28 -0500

    add ejercicio13 functional

---
+++ ejercicios/ejercicio13.py
@@ -0,0 +1,12 @@
+numero_1 = int(input("ingrese el primer numero: "))
+numero_2 = int(input("ingrese el segundo numero: "))
+
+suma = numero_1+numero_2
+resta = numero_1-numero_2
+multiplicacion = numero_1*numero_2
+division = numero_1/numero_2
+
+print(f"la suma de los dos numeros es: {suma}")
+print(f"la resta de los dos numeros es: {resta}")
+print(f"los dos numeros multiplicaods dan como resultado: {multiplicacion}")
+print(f"los dos numeros divididos dan como resultado: {division}")
