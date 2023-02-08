68fa6e77e1f4f808fcfed3ae630eb7e016423f23
Nicolas Tobon <nicolastobon242>
2023-02-08 14:58:45 -0500

    add ejercicio2 functional correction of syntax's in ejercio1

--- ejercicios/ejercicio1.py
+++ ejercicios/ejercicio1.py
@@ -2,7 +2,7 @@ if __name__ == "__main__":
     print()
 
 name = input("ingresa tu nombre: ")
-edad = (int(input("ingresa tu edad: ")))
+edad = int(input("ingresa tu edad: "))
 
 print(f"el nombre ingresado es {name} y la edad ingresada es de {edad} años")
 
---
+++ ejercicios/ejercicio2.py
@@ -0,0 +1,7 @@
+import math
+
+radio = float(input("ingrese el radio del circulo al que desea conocer su area: "))
+
+area = math.pi*(radio**2)
+
+print(f"el area del circulo es {area}")
